ef540dfb161e9d872ca1c6110320686594f6992c
Felipe <felipeshonorato@gmail.com>
2022-12-07 11:24:58 -0300

    Initial commint with two models created, Project and Tasks.

---
+++ src/main/java/Model/Tasks.java
@@ -0,0 +1,105 @@
+package Model;
+
+import java.util.Date;
+
+public class Tasks {
+
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompleted;
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Tasks(String name, String description, String notes, boolean isCompleted, Date deadline, Date createdAt, Date updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.notes = notes;
+        this.isCompleted = isCompleted;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Tasks{" +
+                "id=" + id +
+                ", idProject=" + idProject +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", notes='" + notes + '\'' +
+                ", isCompleted=" + isCompleted +
+                ", deadline=" + deadline +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
